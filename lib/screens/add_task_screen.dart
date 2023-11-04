5dac17d767711b49ee410a75a53e606687c9193b
AndresLince <lynxdarmatron@gmail.com>
2023-11-04 16:04:49 -0500

    Form WIP

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 
 class AddTaskScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xff757575),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          )
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            )),
+        child: Column(
+          children: [
+            Text('asda'),
+            TextField(),
+            MaterialButton(
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+
+              },
+              minWidth: 300,
+              height: 50.0,
+            )
+          ],
         ),
       ),
     );
