e6a675433f7eb7fe4ff370c4051d326ae8d85445
AndresLince <lynxdarmatron@gmail.com>
2023-11-05 08:31:40 -0500

    Add TaskCheckBox Widget

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -10,12 +10,29 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(text),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {
+      trailing: TaskCheckBox(),
+    );
+  }
+}
 
-        },
-      ),
+class TaskCheckBox extends StatefulWidget {
+
+  @override
+  State<TaskCheckBox> createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool? isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
     );
   }
 }
