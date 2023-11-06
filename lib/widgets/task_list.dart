a97cecff174baba785844443b297cfb9a35f65a3
AndresLince <lynxdarmatron@gmail.com>
2023-11-06 16:12:08 -0500

    task list wip

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -21,9 +21,9 @@ class _TasksListState extends State<TasksList> {
 
     return ListView(
       children: [
-        TaskTile(text: 'Tarea 1'),
-        TaskTile(text: 'Tarea 2'),
-        TaskTile(text: 'Tarea 3')
+        TaskTile(taskTitle: tasks[0].name, isChecked: tasks[0].isDone,),
+        TaskTile(taskTitle: tasks[1].name, isChecked: tasks[1].isDone,),
+        TaskTile(taskTitle: tasks[2].name, isChecked: tasks[2].isDone,),
       ],
     );
   }
