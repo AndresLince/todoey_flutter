84ca68a92e6270f71ff4b8e531d71b2f8962699c
AndresLince <lynxdarmatron@gmail.com>
2023-11-06 16:17:46 -0500

    Add list view builder

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -19,12 +19,9 @@ class _TasksListState extends State<TasksList> {
       Task(name: 'Tarea 3'),
     ];
 
-    return ListView(
-      children: [
-        TaskTile(taskTitle: tasks[0].name, isChecked: tasks[0].isDone,),
-        TaskTile(taskTitle: tasks[1].name, isChecked: tasks[1].isDone,),
-        TaskTile(taskTitle: tasks[2].name, isChecked: tasks[2].isDone,),
-      ],
-    );
+    return ListView.builder(itemBuilder: (context, index) {
+      return TaskTile(
+          isChecked: tasks[index].isDone, taskTitle: tasks[index].name);
+    }, itemCount: tasks.length,);
   }
 }
