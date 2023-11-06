e6651b791b9a1fab5c50a4fd471214fa06cf8932
AndresLince <lynxdarmatron@gmail.com>
2023-11-06 16:04:21 -0500

    Add list of tasks

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
+import '../models/task.dart';
+
 class TasksList extends StatelessWidget {
   const TasksList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Task> tasks = [
+      Task(name: 'Tarea 1'),
+      Task(name: 'Tarea 2'),
+      Task(name: 'Tarea 3'),
+    ];
 
     return ListView(
       children: [
