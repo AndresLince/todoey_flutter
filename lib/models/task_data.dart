2055d4f63a56246ca551bc496d42b77efdd4a8b8
AndresLince <lynxdarmatron@gmail.com>
2023-11-20 06:33:21 -0500

    Make tasks variable private

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -2,18 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Tarea 1'),
     Task(name: 'Tarea 2'),
     Task(name: 'Tarea 3'),
   ];
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void add(Task task) {
-    tasks.add(task);
+    _tasks.add(task);
     // This call tells the widgets that are listening to this model to rebuild.
     notifyListeners();
   }
